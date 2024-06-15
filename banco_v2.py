e46a2e00abb2a82aa05a614bb9c22422faf28ffe
Anderson <101288487+Andsu022@users.noreply.github.com>
2024-06-15 10:18:05 -0300

    Add files via upload

--- banco_v2.py
+++ banco_v2.py
@@ -40,20 +40,24 @@ def criar_conta_corrente(numero_cpf, numero_conta, saldo, senha):
     conta_corrente.update({numero_cpf: {'Número da conta': numero_conta,'Agência': NUMERO_AGENCIA, 'Titular': titular, 'Saldo da conta': saldo, 'Senha': senha}})
 
 
-def saque_conta(saldo, valor, numero_conta):
+def saque_conta(valor, numero_conta):
     global quantidade_saques
     global LIMITE_SAQUES
+
+    for value in conta_corrente.values():
+        if numero_conta == value['Número da conta']:
+            saldo = value['Saldo da conta']
+    
 
     if quantidade_saques < 3:
         if saldo >= valor:
             if valor <= LIMITE_SAQUES:
-                saldo -= valor
+                for value in conta_corrente.values():
+                    if numero_conta == value['Número da conta']:
+                        value['Saldo da conta'] -= valor
                 print('Operação realizada com sucesso !!')
                 quantidade_saques += 1
                 extrato_saques.append(valor)
-                for valor in conta_corrente.items():
-                    if numero_conta == valor['Número da conta']:
-                        valor['Saldo da conta'] += valor
 
             else:
                 print('Operação não realizada, limite de valor por saque ultrapassado !')
@@ -62,30 +66,26 @@ def saque_conta(saldo, valor, numero_conta):
     else:
         print('Operação não realizada, limite de saques diários ultrapassado !')
 
-    return saldo
 
 
-def deposito_conta(saldo, valor, numero_conta):
+def deposito_conta(vl, num_conta):
     global LIMITE_DEPOSITO
 
-    if valor > 0 :
-        if valor <= LIMITE_DEPOSITO:
-            saldo += valor
-            extrato_depositos.append(valor)
-            for valor in conta_corrente.items():
-                    if numero_conta == valor['Número da conta']:
-                        valor['Saldo da conta'] -= valor
-
+    if vl > 0 :
+        if vl <= LIMITE_DEPOSITO:
+            for value in conta_corrente.values():
+                    if num_conta == value['Número da conta']:
+                        value['Saldo da conta'] += vl
+            
+            extrato_depositos.append(vl)
             print('Operação realizada com sucesso !!')
         else:
             print(f'Operação não realizada, limite de depósito é de R$ {LIMITE_DEPOSITO}')
     else:
         print('Operação não realizada, não é permitido depositar valores negativos !')
-    
-    return saldo
 
 
-def extrato_conta(numero_conta):
+def extrato_conta(num_conta):
     print('|======EXTRATO SAQUES======|')
     if len(extrato_saques) == 0:
         print('| Nenhum saque foi efetuado !')
@@ -102,9 +102,10 @@ def extrato_conta(numero_conta):
             print(f'|Valor: R$ +{j:.2f}')
 
     print('|==================================')
-    for valor in conta_corrente.items():
-            if numero_conta == valor['Número da conta']:
-               saldo = valor['Saldo da conta'] 
+
+    for value in conta_corrente.values():
+            if num_conta == value['Número da conta']:
+               saldo = value['Saldo da conta'] 
 
     print(f'| Saldo atual da conta: R$ {saldo:.2f}') 
 
@@ -124,10 +125,10 @@ def main():
     menu_conta = '''
         |======CONTA======|
         |                 |
-        | [S] - Saque     |
-        | [D] - Depósito  |
-        | [E] - Extrato   |
-        | [F] - Finalizar |
+        | [1] - Saque     |
+        | [2] - Depósito  |
+        | [3] - Extrato   |
+        | [4] - Finalizar |
         |=================|
         |=> '''
     
@@ -140,114 +141,115 @@ def main():
          '''
 
     while True:
-        opc1 = int(input(menu_principal))
-        match opc1:
+        try:
+            opc1 = int(input(menu_principal))
+            match opc1:
 
-            case 1:
-                try:
-                    nome = input('\nDigite o nome completo: ')
-                    cpf = input('\nDigite o CPF: ')
-                    data_nascimento = input('\nDigite sua data de nascimento no formato DD/MM/AAAA: ')
-                    rua = input('\nDigite seu endereco(rua): ')
-                    numero_casa = int(input('\nDigite seu endereco(numero da casa): '))
-                    bairro = input('\nDigite seu endereco(bairro): ')
-                    cidade = input('\nDigite seu endereco(cidade): ')
-                    estado = input('\nDigite seu endereco(sigla estado): ')
-                    criar_usuario(nome, cpf, data_nascimento, rua, numero_casa, bairro, cidade, estado)
-                    os.system('pause')
-                    os.system('cls')
-                    continue
-                except:
-                    print(erro)
-                    continue
+                case 1:
+                    try:
+                        nome = input('\nDigite o nome completo: ')
+                        cpf = input('\nDigite o CPF: ')
+                        data_nascimento = input('\nDigite sua data de nascimento no formato DD/MM/AAAA: ')
+                        rua = input('\nDigite seu endereco(rua): ')
+                        numero_casa = int(input('\nDigite seu endereco(numero da casa): '))
+                        bairro = input('\nDigite seu endereco(bairro): ')
+                        cidade = input('\nDigite seu endereco(cidade): ')
+                        estado = input('\nDigite seu endereco(sigla estado): ')
+                        criar_usuario(nome, cpf, data_nascimento, rua, numero_casa, bairro, cidade, estado)
+                        os.system('pause')
+                        os.system('cls')
+                        continue
+                    except:
+                        print(erro)
+                        continue
 
-            case 2:
-                try:
-                    numero_cpf = input('\nDigite o CPF: ')
-                    numero_conta = input('\nDigite o número da conta(começando com 1): ')
-                    saldo = float(input('\nDigite um valor inicial para o saldo da conta: '))
-                    senha = int(input('\nDigite uma senha para a conta(somente números): '))
-                    criar_conta_corrente(numero_cpf, numero_conta, saldo, senha)
-                    os.system('pause')
-                    os.system('cls')
-                    continue
-                except:
-                    print(erro)
-                    continue
-            
-            case 3:
-                try:
-                    numero_conta = input('\nDigite o número da conta: ')
-                    senha_conta = int(input('\nDigite a senha: '))
-                    acesso = autenticar_acesso(numero_conta, senha_conta)
-                    os.system('pause')
-                    os.system('cls')
-                except:
-                    print(erro)
-
-                if acesso == True:
-                    while True:
-                        try:
-                            opc2 = input(menu_conta)
-                            match opc2:
-
-                                case 'S':
-                                    if saldo == 0:
-                                        print('Operação não permitida, saldo zerado, por favor faça um depósito !')
-                                        os.system('pause')
-                                        os.system('cls')
-                                    else:
-                                        valor = float(input('Digite um valor para sacar: '))
-                                        saldo = saque_conta(saldo = saldo, valor = valor, numero_conta = numero_conta)
-                                        os.system('pause')
-                                        os.system('cls')
-                                    continue
-
-                                case 'D':
-                                    valor = float(input('Digite um valor para depositar: '))
-                                    saldo = deposito_conta(saldo, valor, numero_conta)
-                                    os.system('pause')
-                                    os.system('cls')
-                                    continue
-
-                                case 'E':
-                                    extrato_conta(numero_conta)
-                                    os.system('pause')
-                                    os.system('cls')
-                                    continue
-                                
-                                case 'F':
-                                    print(finalizar)
-                                    break
-
-                                case _:
-                                    print('Opção inválida, escolha novamente uma operação !')
-                                    os.system('pause')
-                                    os.system('cls')
-                                    continue
-                        except:
-                            os.system('cls')
-                            print(erro)
-                            os.system('pause')
-                            os.system('cls')
-                            
-                elif acesso == None:
-                    print('''
-                        |==CONTA NÃO ENCONTRADA !==|
-                          ''')
+                case 2:
+                    try:
+                        numero_cpf = input('\nDigite o CPF: ')
+                        numero_conta = int(input('\nDigite o número da conta(começando com 1): '))
+                        saldo = float(input('\nDigite um valor inicial para o saldo da conta: '))
+                        senha = int(input('\nDigite uma senha para a conta(somente números): '))
+                        criar_conta_corrente(numero_cpf, numero_conta, saldo, senha)
+                        os.system('pause')
+                        os.system('cls')
+                        continue
+                    except:
+                        print(erro)
+                        continue
                 
-                continue
-            
-            case 4:
-                print(finalizar)
-                os.system('exit')
-                break
+                case 3:
+                    try:
+                        num_conta = int(input('\nDigite o número da conta: '))
+                        senha_conta = int(input('\nDigite a senha: '))
+                        acesso = autenticar_acesso(num_conta, senha_conta)
+                        os.system('pause')
+                        os.system('cls')
+                    except:
+                        print(erro)
+                    
+                    if acesso == True:
+                        while True:
+                            try:
+                                opc2 = int(input(menu_conta))
+                                match opc2:
 
-            case _:
-                print('Opção inválida, escolha novamente uma operação !')
-                os.system('pause')
-                os.system('cls')
-                continue
+                                    case 1:
+                                        vl = float(input('Digite um valor para sacar: '))
+                                        saque_conta(valor = vl, numero_conta = num_conta)
+                                        os.system('pause')
+                                        os.system('cls')
+                                        continue
+
+                                    case 2:
+                                        vl = float(input('Digite um valor para depositar: '))
+                                        deposito_conta(vl, num_conta)
+                                        os.system('pause')
+                                        os.system('cls')
+                                        continue
+
+                                    case 3:
+                                        extrato_conta(num_conta)
+                                        os.system('pause')
+                                        os.system('cls')
+                                        continue
+                                    
+                                    case 4:
+                                        print(finalizar)
+                                        break
+
+                                    case _:
+                                        print('Opção inválida, escolha novamente uma operação !')
+                                        os.system('pause')
+                                        os.system('cls')
+                                        continue
+                            except:
+                                os.system('cls')
+                                print(erro)
+                                os.system('pause')
+                                os.system('cls')
+                                
+                    else:
+                        print('''
+                            |==CONTA NÃO ENCONTRADA !==|
+                            ''')
+                    
+                    continue
+                
+                case 4:
+                    print(finalizar)
+                    os.system('exit')
+                    break
+
+                case _:
+                    print('Opção inválida, escolha novamente uma operação !')
+                    os.system('pause')
+                    os.system('cls')
+                    continue
+        except:
+            os.system('cls')
+            print(erro)
+            os.system('pause')
+            os.system('cls')
 
 
 main()
